50fca3afc0a8ca6cf84c5b4c8451d688846f07e9
yswu1022eng <yswu1022eng@gmail.com>
2017-03-29 21:24:02 +0800

    encapsulation

---
+++ Company/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AAA;
+
+namespace Company
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Employee a = new Employee();
+            Console.Write("請輸入員工名稱:");
+            a.Name = Console.ReadLine();
+
+            Console.Write("請輸入底薪:");
+            try
+            {
+                int baseSalary = int.Parse(Console.ReadLine());
+                a.BaseSalary = baseSalary;
+                Console.WriteLine("這位員工的底薪為{0}", baseSalary);
+            }
+            catch
+            {
+                Console.WriteLine("薪水輸入錯誤");
+                return;
+            }
+
+            Console.Write("請輸入獎金值");
+            try
+            {
+                int benefit = int.Parse(Console.ReadLine());
+                a.Benefit = benefit;
+                Console.WriteLine("這位員工的獎金為{0}", benefit);
+            }
+            catch
+            {
+                Console.WriteLine("獎金輸入錯誤");
+                return;
+            }
+
+            Console.Write("所以你這個月可以領:{0}", a.Salary);
+        }
+    }
+}
---
+++ Employee/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AAA
+{
+    public class Employee
+    {
+        private string name;
+        private int baseSalary;
+        private int benefit;
+        private int salary;
+
+        public string Name { get => name; set => name = value; }
+
+        public int BaseSalary
+        {
+            get { return this.baseSalary; }
+            set
+            {
+                if (value < 22000)
+                {
+                    this.baseSalary = 22000;
+                }
+                else
+                {
+                    this.baseSalary = value;
+                }
+            }
+        }
+
+        public int Benefit
+        {
+            get => Benefit;
+            set
+            {
+                if (value < 0)
+                {
+                    this.benefit = 0;
+                }
+                else
+                {
+                    this.benefit = value;
+                }
+            }
+        }
+
+        public int Salary
+        {
+            get => Salary;
+            set
+            {
+                this.salary = this.baseSalary + this.benefit;
+            }
+        }
+    }
+}
